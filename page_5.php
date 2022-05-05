461450130ce1dac6287414c28583e5c7a2c54547
Marou30 <103744637+Marou30@users.noreply.github.com>
2022-05-05 12:28:10 +0200

    Update page_5.php

--- page_5.php
+++ page_5.php
@@ -8,7 +8,7 @@
 
     <body>
 
-        <?php include('../header.php' ); ?>
+        <?php include('header.php' ); ?>
 
         <div id="banniere_image">
             <div id="banniere_description">
@@ -45,7 +45,7 @@
     
 
     <footer>
-        <?php include('../footer.php' ); ?> 
+        <?php include('footer.php' ); ?> 
     </footer>
 
     </body>
